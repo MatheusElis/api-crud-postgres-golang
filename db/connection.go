79642091e47b12c7911b4c9acb183b1d4917e810
Matheus Elis <matheus.elis.silva@gmail.com>
2024-02-21 10:29:13 -0300

    feat: finish the server of api

--- db/connection.go
+++ db/connection.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 
 	"github.com/MatheusElis/api-crud-postgres-golang/configs"
-  _ "github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 func OpenConnection() (*sql.DB, error){
   conf := configs.GetDB()
 
-  sc := fmt.Sprintf("host=%s port=%s user=%s pass=%s dbname=%s sslmode=disable",
+  sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
     conf.Host, conf.Port, conf.User, conf.Pass, conf.Database)
 
   conn, err := sql.Open("postgres",sc)
